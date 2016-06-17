e1f2e46c1a9f7a95248e4119a6a29b9b50120d9c
Cristiano Betta <cbetta@gmail.com>
2016-06-17 17:18:51 +0100

    Finished example

--- app.js
+++ app.js
@@ -1,30 +1,74 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import parser from 'body-parser';
 import Nexmo from 'nexmo';
 
+// load the environment variables
 dotenv.config();
+
+// initialize express, the template directory, and form parsing
 const app = express();
+app.use(express.static(__dirname));
+app.use(parser.urlencoded({ extended: false }));
+
+// initialize nexmo
 const nexmo = new Nexmo({
   key: process.env.KEY,
   secret: process.env.SECRET
 });
 
-app.get('/', (_, res) => {
-  const message = `
-  Your delivery is scheduled for tomorrow between 8am and 2pm. If you wish to change the delivery date please reply by typing 1, 2 or 3:
+// the delivery options
+const options = [
+  'this Friday',
+  'next Monday',
+  'next Tuesday'
+];
 
-  1. for tomorrow
-  2. for next Monday
-  3. for next Tuesday
-  `
-  nexmo.sms.sendTextMessage(
-    process.env.FROM,
-    process.env.TO,
-    message,
-    () => {
-      res.send('Notification sent!');
-    }
-  );
+// the notification message
+let notification = "Your Nexmo Mail delivery is scheduled for tomorrow between " +
+              "8am and 2pm. If you wish to change the delivery date please " +
+              "reply by typing 1, 2 or 3:\n\n";
+
+options.forEach((option, index) => {
+  notification += `${index+1}. for ${option}\n`;
 });
 
+// a form for submitting phone numbers
+app.get('/', (_, response) => {
+  response.sendFile('index.html');
+});
+
+// notification endpoint
+app.post('/notify', (request, response) => {
+  send(request.body.number, notification);
+  response.send('Notification sent');
+});
+
+
+// the webhook endpoint
+app.get('/response', (request, response) => {
+  // TODO: store this selection somewhere
+  const selection = parseInt(request.query.text) - 1;
+  const day = options[selection];
+
+  let message = "Please select from on of the valid options only.";
+
+  if (day) {
+    message = `Thank you! Your delivery has been rescheduled for ${day}`;
+  }
+
+  send(request.query.msisdn, message);
+  response.send('Response processed');
+});
+
+// simple function around sending a message
+let send = function(number, message) {
+  nexmo.sms.sendTextMessage(
+    process.env.FROM,
+    number,
+    message
+  );
+}
+
+// start the server
 app.listen(3000);
