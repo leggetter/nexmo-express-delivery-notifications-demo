da2d6a9843f480022ee93cb7f6582d5355a5f607
Cristiano Betta <cbetta@gmail.com>
2016-06-17 15:59:19 +0100

    Add message sending

--- app.js
+++ app.js
@@ -1,8 +1,30 @@
+import dotenv from 'dotenv';
 import express from 'express';
-const app = express();
+import Nexmo from 'nexmo';
 
-app.get('/message', (_, res) => {
-  res.send('Message Sent!');
+dotenv.config();
+const app = express();
+const nexmo = new Nexmo({
+  key: process.env.KEY,
+  secret: process.env.SECRET
+});
+
+app.get('/', (_, res) => {
+  const message = `
+  Your delivery is scheduled for tomorrow between 8am and 2pm. If you wish to change the delivery date please reply by typing 1, 2 or 3:
+
+  1. for tomorrow
+  2. for next Monday
+  3. for next Tuesday
+  `
+  nexmo.sms.sendTextMessage(
+    process.env.FROM,
+    process.env.TO,
+    message,
+    () => {
+      res.send('Notification sent!');
+    }
+  );
 });
 
 app.listen(3000);
